48d066afd9ce00c3b6f5547a0632f2d871a91a5d
Mehmet Akif Duba <mail@makifdb.com>
2022-03-08 14:13:38 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,10 @@
 module.exports = {
-    darkMode: 'class',
-
-    content: [
-      "./layouts/**/*.{html,md}",
-      "./content/**/*.{html,md}",
-      "./themes/**/layouts/**/*.{html,md}",
-      "./content/**/layouts/**/*.{html,md}",
-    ],
-    theme: {
-      extend: {}
-    },
-    plugins: [
-      require('@tailwindcss/typography'),
-    ]
-  }
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.{html,md}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
